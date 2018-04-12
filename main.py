b31c6b2f2bd18b42c718f15500774e35a93a17cf
CirQ <cirq999@163.com>
2018-04-12 13:22:35 +0800

    fix bugs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from __future__ import division
 import argparse
 import gc
 import os
+import time
 
 import torch
 import torch.nn as nn
@@ -22,7 +23,7 @@ parser.add_argument('--gpus', default='0', type=str, help='gpu ids (default: 0)'
 
 parser.add_argument('--tag', type=str, required=True, help='the path to tags')
 parser.add_argument('--image', type=str, required=True, help='the path to images')
-parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
+parser.add_argument('--threads', type=int, default=8, help='number of threads for data loader to use')
 parser.add_argument('--batch', type=int, default=64, help='training batch size')
 parser.add_argument('--image_size', type=int, default=128, help='the height and width of images')
 parser.add_argument('--noise_size', type=int, default=128, help='the length of noise vector')
@@ -53,8 +54,8 @@ def main():
 
     print('[INFO] Loading datasets')
     train_set = TagImageDataset(tag_path=opt.tag, img_path=opt.image)
-    # train_loader = DataLoader(train_set, num_workers=opt.threads, batch_size=opt.batch, shuffle=True)
-    train_loader = DataLoader(train_set, batch_size=opt.batch, shuffle=True, drop_last=True)
+    train_loader = DataLoader(train_set, num_workers=opt.threads, batch_size=opt.batch, shuffle=True, drop_last=True)
+    # train_loader = DataLoader(train_set, batch_size=opt.batch, shuffle=True, drop_last=True)
 
     print('[INFO] Building model')
     G = Generator(opt.features)
@@ -95,8 +96,9 @@ def main():
             print('[ERROR] No model found at {}'.format(opt.pre_trained))
 
     print('[INFO] Start Training')
+    start = time.time()
     for epoch in range(opt.start_epoch, opt.epoch+1):
-        train(train_loader, G, D, G_optim, D_optim, criterion, epoch)
+        train(train_loader, G, D, G_optim, D_optim, criterion, epoch, start)
         save_stage(G, D, G_optim, D_optim, epoch)
 
 
@@ -108,7 +110,7 @@ def adjust_learning_rate(optimizer, epoch):
 
 lambda_adv = opt.features
 lambda_gp = 0.5
-def train(train_loader, gen, dis, g_optim, d_optim, criterion, epoch):
+def train(train_loader, gen, dis, g_optim, d_optim, criterion, epoch, start_time):
     adjust_learning_rate(g_optim, epoch)
     adjust_learning_rate(d_optim, epoch)
 
@@ -117,7 +119,7 @@ def train(train_loader, gen, dis, g_optim, d_optim, criterion, epoch):
     tags = Variable(FloatTensor(opt.batch, opt.features))                   # input tags
     labels = Variable(FloatTensor(opt.batch))                               # real probability
     if opt.cuda:
-        X, z, tags, labels = (None for _ in range(4))
+        X, z, tags, labels = X.cuda(), z.cuda(), tags.cuda(), labels.cuda()
 
     for iteration, (tag, img) in enumerate(train_loader, start=1):
         X.data.copy_(img)
@@ -191,7 +193,8 @@ def train(train_loader, gen, dis, g_optim, d_optim, criterion, epoch):
 
         gc.collect()
 
-        print('[%d/%d] [%d/%d] Loss_D: %.4f Loss_G: %.4f Loss_D_Label: %.4f Loss_G_Label: %.4f Loss_D_Tag: %.4f Loss_G_Tag: %.4f' % (epoch, opt.epoch, iteration, len(train_loader), loss_d.data[0], loss_g_gen.data[0], loss_d_real_label.data[0] + loss_d_fake_label.data[0], loss_g_gen_label.data[0], loss_d_real_tag.data[0] + loss_d_fake_tag.data[0], loss_g_gen_tag.data[0]))
+        elapsed = time.time() - start_time
+        print('[%d/%d] [%d/%d] %.4f Loss_D: %.4f Loss_G: %.4f Loss_D_Label: %.4f Loss_G_Label: %.4f Loss_D_Tag: %.4f Loss_G_Tag: %.4f' % (epoch, opt.epoch, iteration, len(train_loader), elapsed, loss_d.data[0], loss_g_gen.data[0], loss_d_real_label.data[0] + loss_d_fake_label.data[0], loss_g_gen_label.data[0], loss_d_real_tag.data[0] + loss_d_fake_tag.data[0], loss_g_gen_tag.data[0]))
 
 def save_stage(gen, dis, gen_optim, dis_optim, epoch):
     if not os.path.exists('checkpoint'):
@@ -216,6 +219,8 @@ def save_stage(gen, dis, gen_optim, dis_optim, epoch):
     tags = Variable(FloatTensor(opt.batch, opt.features))
     z.data.normal_(0, 1)
     tags.data.uniform_(to=1)
+    if opt.cuda:
+        z, tags = z.cuda(), tags.cuda()
     sample = gen(torch.cat((z, tags), 1))
     vutil.save_image(sample.data.view(opt.batch, 3, opt.image_size, opt.image_size), samples_out_path)
     print('[DEMO] samples in epoch {} saved'.format(epoch))
